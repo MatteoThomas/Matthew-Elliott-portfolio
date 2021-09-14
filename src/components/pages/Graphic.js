c1b36f6019369c361383b5b4981d0ea1f0724078
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-09-13 18:00:41 -0600

    graphic design pics added

--- src/components/pages/Graphic.js
+++ src/components/pages/Graphic.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "../../styles/Components.css";
+import crowlers from "../../img/crowlers.png";
+import cards from "../../img/LOFT718.png";
+import records from "../../img/wpRECORDS.jpg";
+import recovery from "../../img/recoverybanner.jpg";
 
 function Graphic() {
   return (
     <div className="graphic">
       {/* <h1>Graphic Design</h1> */}
-      <h2>Photoshop / Illustrator / Blender</h2>
-      <p>I have experience in online promotion, print, and product branding.</p>
+      <h2>Photoshop / Illustrator / Blender / XD</h2>
 
+      <p>I have experience in online promotion, print, and product branding.</p>
       <button className="accordion">
-        <a href="https://matteoelliott.myportfolio.com">Check it out here!</a>
+        <a href="https://matteoelliott.myportfolio.com">
+          See my portfolio here
+        </a>
       </button>
+
+      <img id="crowlers" src={crowlers} alt="crowlers" />
+      <img id="records" src={records} alt="records" />
+      <img id="recovery" src={recovery} alt="recovery banner" />
+      <img id="cards" src={cards} alt="crowlers" />
+
       <div className="panel">
         <p></p>
       </div>
